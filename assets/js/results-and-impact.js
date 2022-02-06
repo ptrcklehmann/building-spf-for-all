c24b7508f57f8b892853e42eb84e45445c1e923e
Patrick Lehmann <ptrcklehmann@gmail.com>
2022-02-06 22:29:22 +0100

    results and impact

---
+++ assets/js/results-and-impact.js
@@ -0,0 +1,333 @@
+Chart.defaults.font.size=14;
+Chart.defaults.font.family='Noto Sans','sans-serif';
+Chart.defaults.borderWidth=0
+
+Chart.register(ChartDataLabels);
+const labels=[
+    'Step 1',
+    'Step 2',
+    'Step 3'
+];
+
+const data={
+    labels: labels,
+    datasets: [
+        {
+            label: 'Achievement 2020',
+            backgroundColor: '#1E2DBE',
+            data: [10,13,16],
+            datalabels: {
+                color: '#fff'
+            }
+        },{
+            label: 'Ambition 2025',
+            backgroundColor: '#05D2D2',
+            data: [20,30,40],
+            datalabels: {
+                color: '#230050'
+            }
+        }]
+};
+let delayed;
+
+const config={
+    type: 'bar',
+    data: data,
+    options: {
+        animation: {
+            onComplete: () => {
+                delayed=true;
+            },
+            delay: (context) => {
+                let delay=0;
+                if(context.type==='data'&&context.mode==='default'&&!delayed) {
+                    delay=context.dataIndex*600+context.datasetIndex*300;
+                }
+                return delay;
+            },
+        },
+        plugins: {
+            title: {
+                display: false,
+                // color: '#230050',
+                // text: 'Number of institutional changes achieved in countries',
+                // font: {
+                //     size: 16,
+                // }
+            },
+            legend: {
+                display: false,
+                align: 'end'
+            },
+            tooltip: {
+                backgroundColor: '#1E2DBE'
+            }
+        },
+        responsive: true,
+        scales: {
+            x: {
+                stacked: true,
+
+                grid: {
+                    display: false
+                }
+            },
+            y: {
+                stacked: true,
+                display: false,
+                grid: {
+                    display: false
+                }
+            }
+        },
+        grid: {
+            gridLineOptions: {
+                display: false
+            }
+        }
+
+    }
+
+};
+
+const impactData1={
+    labels: ['Legal coverage'],
+    datasets: [
+        {
+            label: 'Ambition 2025',
+            backgroundColor: '#05D2D2',
+            borderWidth: 0,
+            data: [20],
+            datalabels: {
+                color: '#230050',
+                position: 'center'
+            }
+        },{
+            label: 'Achievement 2020',
+            backgroundColor: '#1E2DBE',
+            borderWidth: 0,
+            data: [11.6],
+            datalabels: {
+                color: '#fff',
+                position: 'center'
+            }
+        }]
+}
+const impactConfig1={
+    type: 'pie',
+    data: impactData1,
+    options: {
+        animation: {
+            onComplete: () => {
+                delayed=true;
+            },
+            delay: (context) => {
+                let delay=0;
+                if(context.type==='data'&&context.mode==='default'&&!delayed) {
+                    delay=context.dataIndex*600+context.datasetIndex*300;
+                }
+                return delay;
+            },
+        },
+        plugins: {
+            title: {
+                display: false,
+            },
+            legend: {
+                display: false,
+            },
+            labels: {
+                display: false
+            },
+            tooltip: {
+                backgroundColor: '#1E2DBE'
+            }
+        },
+        responsive: true,
+        scales: {
+            x: {
+                display: false,
+                stacked: true,
+                grid: {
+                    display: false,
+                }
+            },
+            y: {
+                stacked: true,
+                display: false,
+                grid: {
+                    display: false
+                }
+            }
+        },
+        grid: {
+            gridLineOptions: {
+                display: false
+            }
+        }
+
+    }
+
+}
+const impactData2={
+    labels: ['Effective coverage'],
+    datasets: [
+        {
+            label: 'Ambition 2025',
+            backgroundColor: '#05D2D2',
+            borderWidth: 0,
+            data: [30],
+            datalabels: {
+                color: '#230050'
+            }
+        },{
+            label: 'Achievement 2020',
+            backgroundColor: '#1E2DBE',
+            borderWidth: 0,
+            data: [10.9],
+            datalabels: {
+                color: '#fff'
+            }
+        }]
+}
+const impactConfig2={
+    type: 'pie',
+    data: impactData2,
+    options: {
+        animation: {
+            onComplete: () => {
+                delayed=true;
+            },
+            delay: (context) => {
+                let delay=0;
+                if(context.type==='data'&&context.mode==='default'&&!delayed) {
+                    delay=context.dataIndex*600+context.datasetIndex*300;
+                }
+                return delay;
+            },
+        },
+        plugins: {
+            title: {
+                display: false,
+            },
+            legend: {
+                display: false,
+            },
+            labels: {
+                display: false
+            },
+            tooltip: {
+                backgroundColor: '#1E2DBE'
+            }
+        },
+        responsive: true,
+        scales: {
+            x: {
+                display: false,
+                stacked: true,
+                grid: {
+                    display: false,
+                }
+            },
+            y: {
+                stacked: true,
+                display: false,
+                grid: {
+                    display: false
+                }
+            }
+        },
+        grid: {
+            gridLineOptions: {
+                display: false
+            }
+        }
+
+    }
+
+};
+const impactData3={
+    labels: ['More comprehensive and adequate protection'],
+    datasets: [
+        {
+            label: 'Ambition 2025',
+            backgroundColor: '#05D2D2',
+            borderWidth: 0,
+            data: [10],
+            datalabels: {
+                color: '#230050'
+            }
+        },{
+            label: 'Achievement 2020',
+            backgroundColor: '#1E2DBE',
+            borderWidth: 0,
+            data: [2.6],
+            datalabels: {
+                color: '#fff'
+            }
+        }]
+}
+const impactConfig3={
+    type: 'pie',
+    data: impactData3,
+    options: {
+        animation: {
+            onComplete: () => {
+                delayed=true;
+            },
+            delay: (context) => {
+                let delay=0;
+                if(context.type==='data'&&context.mode==='default'&&!delayed) {
+                    delay=context.dataIndex*600+context.datasetIndex*300;
+                }
+                return delay;
+            },
+        },
+        plugins: {
+            title: {
+                display: false,
+            },
+            legend: {
+                display: false,
+            },
+            labels: {
+                display: false
+            },
+            tooltip: {
+                backgroundColor: '#1E2DBE'
+            }
+        },
+        responsive: true,
+        scales: {
+            x: {
+                display: false,
+                stacked: true,
+                grid: {
+                    display: false,
+                }
+            },
+            y: {
+                stacked: true,
+                display: false,
+                grid: {
+                    display: false
+                }
+            }
+        },
+        grid: {
+            gridLineOptions: {
+                display: false
+            }
+        }
+
+    }
+
+};
+
+const institutionalChangesChart=new Chart(
+    document.getElementById('institutional-changes'),
+    config
+)
+const impactOnPeopleChart1st=new Chart(document.getElementById('impact-on-people-1st'),impactConfig1)
+const impactOnPeopleChart2nd=new Chart(document.getElementById('impact-on-people-2nd'),impactConfig2)
+const impactOnPeopleChart3rd=new Chart(document.getElementById('impact-on-people-3rd'),impactConfig3)
