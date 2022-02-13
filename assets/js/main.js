068ecbfe9586a72b3eadab10bee9b97952615187
Patrick Lehmann <ptrcklehmann@gmail.com>
2022-02-13 23:29:52 +0100

    added meta tags to thematic pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,18 @@
     }
   })
 
+  const currentLocation = location.href;
+  console.log(currentLocation, 'currentLocation')
+  const menuItem = document.querySelectorAll('.main-menu ul li a')
+  console.log(menuItem, 'menu items')
+  const menuLength = menuItem.length
+  for (let i = 0; i<menuLength; i++) {
+    console.log(menuItem[i].href, 'menuItem[i].href')
+    if(menuItem[i].href === currentLocation) {
+      menuItem[i].classList.add('active')
+    } else menuItem[i].classList.remove('active')
+  }
+
 
   /*--}
       Custom script to call Background
