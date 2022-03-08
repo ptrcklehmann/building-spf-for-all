6661f95d1756f218428aa3f67251ebc9dc2416fb
patrick lehmann <ptrcklehmann@gmail.com>
2022-03-08 12:45:59 +0100

    update partnerships page

--- assets/js/lib/library.js
+++ assets/js/lib/library.js
@@ -2,3 +2,9 @@ customSelect('.form-select', {
     openerClass: 'custom-select-opener library',
     panelClass: 'custom-select-panel library',
 });
+var pckry = new Packery( '.publications-grid', {
+    itemSelector: '.publication-item',
+    gutter: 17,
+    percentPosition: true,
+    transitionDuration: '0.3s'
+  });
